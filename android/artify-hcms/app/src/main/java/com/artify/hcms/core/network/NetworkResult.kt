78de16b9a05e1745dd4504f10e9d35c22dbf5b15
usmanclaude92-creator <usmanclaude92@gmail.com>
2026-09-09 19:28:04 +0400

    Delete android/artify-hcms directory

--- android/artify-hcms/app/src/main/java/com/artify/hcms/core/network/NetworkResult.kt
+++
@@ -1,7 +0,0 @@
-package com.artify.hcms.core.network
-
-sealed class NetworkResult<out T> {
-    data class Success<out T>(val data: T) : NetworkResult<T>()
-    data class Error(val message: String, val code: Int? = null) : NetworkResult<Nothing>()
-    data object Loading : NetworkResult<Nothing>()
-}
